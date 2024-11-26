d8473a2991a56b3d192a007dc08c11e2d2e1ad17
RomanKhal <haluev.roman@mail.ru>
2024-11-27 00:06:38 +0300

    add ParityChecker<T> parityChecker parameter into merge() method, move sort code into sortEvenValues, sort method now use optional argument

--- src/main/java/data/sort/MergeSort.java
+++ src/main/java/data/sort/MergeSort.java
@@ -10,51 +10,70 @@ public class MergeSort<T> implements SortAlgorithm<T> {
 
     @Override
     public List<T> sort(List<T> data, Comparator<T> comparator) {
-        if (data == null) return null;
-        if (data.size() < 2) return data;
+        return sortEvenValues(data, comparator, null);
+    }
+
+    @Override
+    public List<T> sortEvenValues(List<T> data, Comparator<T> comparator, ParityChecker<T> parityChecker) {
+        if (data == null) return new ArrayList<>();
+        if (data.size() < 2) return new ArrayList<>(data);
         int mid = data.size() / 2;
         List<T> left = new ArrayList<>(data.subList(0, mid));
         List<T> right = new ArrayList<>(data.subList(mid, data.size()));
 
         left = sort(left, comparator);
         right = sort(right, comparator);
-        return merge(left, right, comparator);
+        return merge(left, right, comparator, parityChecker);
     }
 
-    private List<T> merge(List<T> a, List<T> b, Comparator<T> comparator) {
-        int pA = 0;
-        int pB = 0;
-        int al = a.size();
-        int bl = b.size();
+    private List<T> merge(List<T> left, List<T> right, Comparator<T> comparator, ParityChecker<T> parityChecker) {
+        int leftPointer = 0;
+        int rightPointer = 0;
+        int leftLen = left.size();
+        int rightLen = right.size();
+        boolean parityCheckerFlag = parityChecker != null;
         List<T> res = new ArrayList<>();
+        List<T> resImposter = new ArrayList<>();
 
-        while (pA < al && pB < bl) {
-            T e1 = a.get(pA);
-            T e2 = b.get(pB);
+        while (leftPointer < leftLen && rightPointer < rightLen) {
+            T e1 = left.get(leftPointer);
+            T e2 = right.get(rightPointer);
+            if (parityCheckerFlag) {
+                if (!parityChecker.isEven(e1)) {
+                    res.set(leftPointer++, e1);
+                    e1 = left.get(leftPointer);
+                }
+                if (!parityChecker.isEven(e2)) {
+                    resImposter.set(rightPointer++, e2);
+                    e2 = right.get(rightPointer);
+                }
+            }
             int compare = comparator.compare(e1, e2);
             if (compare == 0) {
-                res.add(a.get(pA++));
-                res.add(b.get(pB++));
+                res.add(e1);
+                leftPointer++;
+                res.add(e2);
+                rightPointer++;
                 continue;
             }
             if (compare > 0) {
-                res.add(a.get(pA++));
-            } else res.add(b.get(pB++));
+                res.add(e1);
+                leftPointer++;
+            } else {
+                res.add(e2);
+                rightPointer++;
+            }
         }
 
-        while (pA < al) {
-            res.add(a.get(pA++));
+        res.addAll(resImposter);
+
+        while (leftPointer < leftLen) {
+            res.add(left.get(leftPointer++));
         }
 
-        while (pB < bl) {
-            res.add(b.get(pB++));
+        while (rightPointer < rightLen) {
+            res.add(right.get(rightPointer++));
         }
-
         return res;
-    }
-
-    @Override
-    public List<T> sortEvenValues(List<T> data, Comparator<T> comparator, ParityChecker<T> parityChecker) {
-        return List.of();
     }
 }
