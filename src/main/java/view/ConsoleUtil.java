dd8b7f0cc1bb05c13e7817df7e563016be4f3990
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2024-11-26 05:13:49 +0500

    Console.getValue method generic now

--- src/main/java/view/ConsoleUtil.java
+++ src/main/java/view/ConsoleUtil.java
@@ -2,37 +2,60 @@ package view;
 
 import data.validate.Validate;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleUtil {
+    private static final List<Class<?>> INPUT_COMPATIBLE_CLASSES = List.of(Integer.class, Double.class, String.class);
     public static final int DEFAULT_READ_ATTEMPTS = 3;
 
-    public static Double getDoubleValue(String requestText) {
-        return getDoubleValue(requestText, null, null);
+
+    public static <T> T getValue(Class<T> cls,
+                                 String requestText) {
+        return getValue(cls, requestText, null, null);
     }
 
-    public static Double getDoubleValue(String requestText,
-                                        Validate<Double> validate,
-                                        String invalidText) {
-        return getDoubleValue(requestText, DEFAULT_READ_ATTEMPTS, validate, invalidText);
+    public static <T> T getValue(Class<T> cls,
+                                 String requestText,
+                                 Validate<T> validate,
+                                 String invalidText) {
+        return getValue(cls, requestText, DEFAULT_READ_ATTEMPTS, validate, invalidText);
     }
 
-    public static Double getDoubleValue(String requestText,
-                                        int readAttempts,
-                                        Validate<Double> validate,
-                                        String invalidText) {
+
+
+    public static <T> T getValue(Class<T> cls,
+                                 String requestText,
+                                 int readAttempts,
+                                 Validate<T> validate,
+                                 String invalidText) {
+        if(!INPUT_COMPATIBLE_CLASSES.contains(cls))
+            throw new IllegalArgumentException(cls.toString() + " is not compatible to use in getValue");
+
+        T result = null;
+
         try(Scanner scanner = new Scanner(System.in)) {
             for(int attempt=0; attempt<readAttempts; attempt++) {
                 System.out.println(requestText);
-                Double result = scanner.nextDouble();    
-                
-                if(validate != null && invalidText != null) 
-                    if(validate.isValid(result)) 
-                        return result;
-                    else
+                String line = scanner.nextLine();
+
+                if(cls.isAssignableFrom(Integer.class))
+                    result = (T) Integer.valueOf(line);
+                else if(cls.isAssignableFrom(Double.class))
+                    result = (T) Double.valueOf(line);
+                else if(cls.isAssignableFrom(String.class))
+                    result = (T) line;
+
+                if(validate != null && invalidText != null)
+                    if(!validate.isValid(result)) {
                         System.out.println(invalidText);
+                        result = null;
+                    }
+                    else
+                        break;
             }
         }
-        return null;
+
+        return result;
     }
 }
