5fd408270a242e5971175e302b013aee291943b6
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2024-11-27 10:42:26 +0500

    create CarStrategy.collectInputData realisation

--- src/main/java/strategy/CarStrategy.java
+++ src/main/java/strategy/CarStrategy.java
@@ -1,11 +1,66 @@
 package strategy;
 
 import data.entities.Car;
+import util.enums.CarFieldEnum;
+
+import java.util.Objects;
+
+import static util.ConsoleUtil.getValue;
 
 public class CarStrategy extends AbstractStrategy<Car> implements Strategy {
     @Override
     public boolean collectInputData(int amount) {
-        return false;
+        int carCount = 0;
+
+        do {
+            String model;
+            int power, productionYear;
+            String strUserInput;
+            Integer intUserInput;
+
+            System.out.println("\nЗаполните машину №" + carCount);
+
+            strUserInput = getValue(String.class, CarFieldEnum.MODEL.getLocaleName(),
+                    Objects::nonNull, "Модель неверная");
+
+            if(strUserInput == null) {
+                System.out.println("Не удалось считать модель, ввод объекта будет пропущен");
+                continue;
+            }
+            else
+                model = strUserInput;
+
+            intUserInput = getValue(Integer.class, CarFieldEnum.POWER.getLocaleName(),
+                    Objects::nonNull, "Мощность неверна");
+
+            if (intUserInput == null) {
+                System.out.println("Не удалось считать мощность, ввод объекта будет пропущен");
+                continue;
+            }
+            else
+                power = intUserInput;
+
+            intUserInput = getValue(Integer.class, CarFieldEnum.YEAR.getLocaleName(),
+                    Objects::nonNull, "Год выпуска неверен");
+
+            if (intUserInput == null) {
+                System.out.println("Не удалось считать год выпуска, ввод объекта будет пропущен");
+                continue;
+            }
+            else
+                productionYear = intUserInput;
+
+            Car car = new Car.CarBuilder()
+                    .setModel(model)
+                    .setPower(power)
+                    .setProductionYear(productionYear)
+                    .build();
+
+            this.rawData.add(car);
+            carCount++;
+        } while (carCount < amount);
+
+        return true;
     }
 
     @Override
