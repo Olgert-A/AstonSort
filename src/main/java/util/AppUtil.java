6ed710d5d30686015bc814baf92d2374a35c73b4
Oleg Andrishunas <106367421+Olgert-A@users.noreply.github.com>
2024-11-27 14:28:01 +0500

    fix signature. SearchAlgorithm must return value, not list

--- src/main/java/util/AppUtil.java
+++ src/main/java/util/AppUtil.java
@@ -4,6 +4,8 @@ import data.search.SearchAlgorithm;
 import data.sort.SortAlgorithm;
 import data.util.ParityChecker;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -12,18 +14,47 @@ public class AppUtil {
         return new SortAlgorithm<T>() {
             @Override
             public List<T> sort(List<T> data, Comparator<T> comparator) {
-                return List.of();
+                List<T> result = new ArrayList<>(data);
+                result.sort(comparator);
+                return result;
             }
 
             @Override
             public List<T> sortEvenValues(List<T> data, Comparator<T> comparator, ParityChecker<T> parityChecker) {
-                return List.of();
+                List<T> even = new ArrayList<>();
+                List<Integer> evenIndexes = new ArrayList<>();
+
+                for (int i = 0; i < data.size(); i++) {
+                    T obj = data.get(i);
+                    if(parityChecker.isEven(obj)) {
+                        even.add(obj);
+                        evenIndexes.add(i);
+                    }
+                }
+
+                List<T> sortedEven = sort(even, comparator);
+                List<T> result = new ArrayList<>(data);
+
+                for (int i = 0; i < sortedEven.size(); i++) {
+                    T obj = sortedEven.get(i);
+                    int setIndex = evenIndexes.get(i);
+                    result.set(setIndex,obj);
+                }
+
+                return result;
             }
         };
     }
 
     public static <T> SearchAlgorithm<T> getSearchAlgorithm() {
-        return (data, searchCondition, comparator) -> List.of();
+        return (data, searchCondition, comparator) -> {
+            int index = Collections.binarySearch(data, searchCondition, comparator);
+            T found = null;
+            if(index>=0)
+                found = data.get(index);
+
+            return found;
+        };
     }
 
 }
